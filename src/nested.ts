82cb8e69bb7059725d4fdaccf54affb3dffd73ab
Ben Wootten <bwootten@udel.edu>
2025-02-28 12:05:30 -0500

    implemented toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -95,7 +95,20 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    return (
+        "id,name,options,points,published\n" +
+        questions
+            .map((question: Question): string =>
+                [
+                    question.id.toString(),
+                    question.name,
+                    question.options.length.toString(),
+                    question.points.toString(),
+                    question.published.toString(),
+                ].join(","),
+            )
+            .join("\n")
+    );
 }
 
 /**
