8aa19a7d34d8fad6fe3504c8d131dd5c6b9408b2
Ben Wootten <bwootten@udel.edu>
2025-03-07 11:01:05 -0500

    implemented ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    function swapType(): void {
+        if (type === "multiple_choice_question") {
+            setType("short_answer_question");
+        } else {
+            setType("multiple_choice_question");
+        }
+    }
+
+    return (
+        <span>
+            <Button onClick={swapType}>Change Type</Button>{" "}
+            {type === "short_answer_question"
+                ? "Short Answer"
+                : "Multiple Choice"}
+        </span>
+    );
 }
